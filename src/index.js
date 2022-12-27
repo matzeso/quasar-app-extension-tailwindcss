0c7798fdba054888020c83728961ef8fa0eca8fa
Thijs Van der Schaeghe <thijs@catlab.be>
2022-12-27 11:34:05 +0100

    Refactor package to not call purge-css manually, but instead relying on tailwindcss to handle that. This way any purgecss configuration set in tailwind.config.js will be handled correctly.
    Also added default configuration for when purge config is not set.

--- src/index.js
+++ src/index.js
@@ -15,40 +15,65 @@ module.exports = function (api) {
     api.compatibleWith('@quasar/app', '^1.0.0 || ^2.0.0 || ^3.0.0-beta')
     // api.compatibleWith('postcss', '^8.1.0') // using compat build for now
 
-    const purgecss = require('@fullhuman/postcss-purgecss')({
+    const tailwindConfigFile = api.resolve.src('extensions/tailwindcss/tailwind.config.js');
 
-        // Specify the paths to all of the template files in your project
-        content: [
-            api.resolve.src('**/*.html'),
-            api.resolve.src('**/*.vue'),
-        ],
+    // tailwind.config.js to see if we should set purge-css config ourselves
+    let tailwindConfig = {};
+    try {
+        tailwindConfig = require(tailwindConfigFile);
+    } catch (e) {
+        // no worries, perfectly fine.
+    }
 
-        // Include any special characters you're using in this regular expression
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
+    let purgecssConfig;
+    let purgecssContent = [
+        api.resolve.src('**/*.html'),
+        api.resolve.src('**/*.vue'),
+    ];
+
+    // purge config found; check if we need to alter any settings
+    if (typeof(tailwindConfig.purge) !== 'undefined') {
+        purgecssConfig = tailwindConfig.purge;
+
+        if (typeof(purgecssConfig.content) === 'undefined') {
+            purgecssConfig.content = purgecssContent;
+        } else {
+            purgecssConfig.content = purgecssConfig.content.map(api.resolve.src);
+        }
+
+        // by default, only enable purge on production
+        if (typeof(purgecssConfig) === 'undefined') {
+            tailwindConfig.purge.enabled = api.ctx.prod;
+        }
+    } else {
+        // use default purge config
+        purgecssConfig = {
+            enabled: api.ctx.prod,
+            content: purgecssContent
+        };
+    }
+
+    purgecssConfig.defaultExtractor = content => content.match(/[\w-/:]+(?<!:)/g) || [];
 
     api.chainWebpack((cfg, {isClient, isServer}, api) => {
         const plugins = [
-            require('tailwindcss')(api.resolve.src('extensions/tailwindcss/tailwind.config.js')),
+            require('tailwindcss')(tailwindConfigFile),
             require('autoprefixer'),
         ];
-        if (api.ctx.prod) {
-            plugins.push(purgecss);
-        }
 
         cfg.module
             .rule('tailwind')
-                .test(/\.css$/)
-                .include
-                    .add(api.resolve.src('extensions/tailwindcss'))
-                    .end()
-                .use('postcss')
-                    .loader('postcss-loader')
-                    .options({postcssOptions: {
-                        ident: 'postcss',
-                        plugins: plugins
-                    }})
-                    .end()
+            .test(/\.css$/)
+            .include
+            .add(api.resolve.src('extensions/tailwindcss'))
+            .end()
+            .use('postcss')
+            .loader('postcss-loader')
+            .options({postcssOptions: {
+                    ident: 'postcss',
+                    plugins: plugins
+                }})
+            .end()
     })
 
     api.extendQuasarConf(extendConf)
